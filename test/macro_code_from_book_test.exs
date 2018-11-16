c1fa66a5620b57af2988420ae7573937eacde4ad
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-16 11:34:58 -0500

    pg12, recreate the unless command

--- test/macro_code_from_book_test.exs
+++ test/macro_code_from_book_test.exs
@@ -1,8 +1,11 @@
 defmodule MacroCodeFromBookTest do
   use ExUnit.Case
   doctest MacroCodeFromBook
-  require Math
 
+
+
+
+  require Math
   test "ch1 macro, look at pieces of a quoted expression" do
     assert Math.say(5 + 2) ==
       [result: 7, str: "5 plus 2 is 7"]
@@ -14,5 +17,11 @@ defmodule MacroCodeFromBookTest do
   test "ch1 macro, this works too! but 8*4 will already have been evaluated" do
     assert Math.say(8 * 4 + 2) ==
       [result: 34, str: "32 plus 2 is 34"]
+  end
+
+  require ControlFlow
+  test "ch1 macro, recreating the unless macro" do
+    assert ControlFlow.unless 2 == 5, do: "block entered" ==
+      "block entered"
   end
 end
