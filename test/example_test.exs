8b83cf4f6b101c289b3eb043703a0908bc53dfb8
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-11-16 13:39:19 -0500

    example test file for rapid building of future tests

---
+++ test/example_test.exs
@@ -0,0 +1,14 @@
+defmodule ExampleTest do
+  use ExUnit.Case
+  # doctest Example
+
+  # require Example
+  test "ch #, assert example" do
+    assert 1 === 1
+  end
+
+  test "ch #, refute example" do
+    assert 1 === 1
+  end
+
+end
